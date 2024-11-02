73788ad85ce5022be9d49626c913768bd250fa8a
tag4 <3322323360@qq.com>
2024-11-02 17:53:01 +0800

    2

--- b.py
+++ b.py
@@ -9,4 +9,4 @@ letters = string.ascii_uppercase  # 'A' 到 'Z'
 for _ in range(3):  # 假设我们输出5个随机字母
     random_letter = random.choice(letters)  # 从字母表中随机选择一个字母
     print(random_letter)  # 输出随机字母
-    time.sleep(5)  # 等待5秒钟
+    time.sleep(5)  # 等待5秒钟,
